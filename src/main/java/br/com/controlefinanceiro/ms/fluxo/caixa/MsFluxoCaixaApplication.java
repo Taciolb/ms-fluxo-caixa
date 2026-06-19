af2b59a406aacde83de24f6c8c1a7193ea37d865
Taciano <taciolb@gmail.com>
2026-06-19 20:48:54 -0300

    feat: habilita OpenFeign na aplicacao

--- src/main/java/br/com/controlefinanceiro/ms/fluxo/caixa/MsFluxoCaixaApplication.java
+++ src/main/java/br/com/controlefinanceiro/ms/fluxo/caixa/MsFluxoCaixaApplication.java
@@ -2,11 +2,14 @@ package br.com.controlefinanceiro.ms.fluxo.caixa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsFluxoCaixaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MsFluxoCaixaApplication.class, args);
 	}
 
