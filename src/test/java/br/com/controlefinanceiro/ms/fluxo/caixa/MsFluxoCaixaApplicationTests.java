f68bd18a53d56a528432d12790a61291f1b7a7c1
Taciano <taciolb@gmail.com>
2026-06-19 21:22:16 -0300

    fix: add propriedades de teste via TestPropertySource

--- src/test/java/br/com/controlefinanceiro/ms/fluxo/caixa/MsFluxoCaixaApplicationTests.java
+++ src/test/java/br/com/controlefinanceiro/ms/fluxo/caixa/MsFluxoCaixaApplicationTests.java
@@ -2,8 +2,14 @@ package br.com.controlefinanceiro.ms.fluxo.caixa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"ms-lancamentos.url=http://localhost:8083",
+		"jwt.secret=minha-chave-secreta-super-segura-financeiro-2026",
+		"jwt.expiration=86400000"
+})
 class MsFluxoCaixaApplicationTests {
 
 	@Test
